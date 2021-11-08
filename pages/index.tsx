69042db026def9676ce0fa2f0603f17af22cd027
Criiis <cristianocaires95@gmail.com>
2021-11-08 22:14:43 +0000

    test the new eslint warnings

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import Title from './components/Title'
 const Home: NextPage = () => {
   console.log('hello world!')
   const hello: string = 'yo'
+  console.log(hello)
 
   return (
     <div className={styles.container}>
