d78c7f0cb8eb5edb2e2d4c66b9c1fa6eee172838
Criiis <cristianocaires95@gmail.com>
2021-11-08 21:45:20 +0000

    add prettier and eslint files

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'eslint:recommended',
+    'next',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 13,
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'no-unused-vars': 0,
+    '@typescript-eslint/no-unused-vars': 0,
+    'no-undef': 2,
+    '@typescript-eslint/no-extra-semi': 'off',
+  },
+}
