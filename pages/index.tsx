7f91ad7400c25af8aab5dff7c76da3108a395e03
Criiis <cristianocaires95@gmail.com>
2021-11-17 22:25:45 +0000

    change index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,9 @@
 import type { NextPage } from 'next'
-import Title from './components/Title'
 
 const Home: NextPage = () => {
-  console.log('hello world!')
-  const hello: string = 'yo'
-  console.log(hello)
-
   return (
     <>
-      <main>
-        <Title title='Hello Index!' />
-      </main>
+      <h1>index</h1>
     </>
   )
 }
