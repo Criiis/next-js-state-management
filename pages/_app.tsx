71160be8a5de0ae7d0e6347d613eeda22b8d5450
Criiis <cristianocaires95@gmail.com>
2021-11-28 20:16:20 +0000

    wrapo the function in cart provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,15 @@ import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
 import HeadPage from './components/Head'
 import Menu from './components/Menu'
+import { CartProvider } from 'react-use-cart'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <CartProvider>
       <HeadPage />
       <Menu />
       <Component {...pageProps} />
-    </>
+    </CartProvider>
   )
 }
 
