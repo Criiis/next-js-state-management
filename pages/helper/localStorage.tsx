25f2d9a69dd2824eb055e3d752211c78a59139f8
Criiis <cristianocaires95@gmail.com>
2021-12-07 23:44:36 +0000

    updating local storage files with new types

--- pages/helper/localStorage.tsx
+++ pages/helper/localStorage.tsx
@@ -1,28 +1,24 @@
 import { useState } from 'react'
-import contextProducts from './CartProvider.d'
-
-type localStorageType = contextProducts[] | []
+import { stateType } from './CartProvider.d'
 
 export const useSetLocalStorage = (
   key: string,
   initialValue: [] | string
-): [localStorageType, Function] => {
+): [stateType, Function] => {
   //getting initial value of cart products
-  const [getLocalStorage, setGetLocalStorage] = useState<localStorageType>(
-    () => {
-      try {
-        //1.get values from local storage with specific key
-        const item = localStorage.getItem(key)
-        //2.check if value exist if exist -> return as initial value for our "react context" otherwise -> return the 'initial value parameter'
-        return item ? JSON.parse(item) : initialValue
-      } catch {
-        return initialValue
-      }
+  const [getLocalStorage, setGetLocalStorage] = useState<stateType>(() => {
+    try {
+      //1.get values from local storage with specific key
+      const item = localStorage.getItem(key)
+      //2.check if value exist if exist -> return as initial value for our "react context" otherwise -> return the 'initial value parameter'
+      return item ? JSON.parse(item) : initialValue
+    } catch {
+      return initialValue
     }
-  )
+  })
 
   //set value in local storage
-  const setValue = (value: localStorageType | Function) => {
+  const setValue = (value: stateType | Function) => {
     try {
       //1.check if the 'value parameter' is a function or just a value
       const valueToStore =
