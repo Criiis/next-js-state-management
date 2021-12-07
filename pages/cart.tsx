a55e0ffcd4b04ed43ab12b7bc835ace1907d1875
Criiis <cristianocaires95@gmail.com>
2021-12-07 23:45:57 +0000

    update cart page with new remove funcionality

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -1,26 +1,21 @@
 import type { NextPage } from 'next'
 import Link from 'next/dist/client/link'
-import { useCart, useDispatchCart } from './helper/CartProvider'
-import contextProducts, { DispatchContext } from './helper/CartProvider.d'
+import { useCart } from './helper/CartProvider'
+import { contextProducts } from './helper/CartProvider.d'
 
 const Cart: NextPage = () => {
-  const items: contextProducts[] = useCart()
-
-  const dispatch: DispatchContext = useDispatchCart()
-  const removeToCart = (index: number): void => {
-    dispatch({ type: 'REMOVE', index })
-  }
+  const { state, removeItem } = useCart()
 
   return (
     <>
       <h1>cart page</h1>
       <h2>what can do?!</h2>
-      {items.map((el: any, i: any) => (
+      {state.map((el: contextProducts, i: number) => (
         <div key={i}>
           <img loading='lazy' src={el.image} alt={el.title} width='150px' />
           <p>{el.title}</p>
-          <p>{el.price}</p>
-          <button onClick={() => removeToCart(i)}>remove from cart</button>
+          <p>{el.price} £</p>
+          <button onClick={() => removeItem(i)}>remove from cart</button>
           <br />
           <Link href='/product/[id]' as={`/product/${el.id}`}>
             <a>See product</a>
