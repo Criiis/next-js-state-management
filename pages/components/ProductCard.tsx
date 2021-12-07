10820b5ffdd57fb6fdb81e389681cb40501b837e
Criiis <cristianocaires95@gmail.com>
2021-12-07 23:46:38 +0000

    update product card with new funcionality

--- pages/components/ProductCard.tsx
+++ pages/components/ProductCard.tsx
@@ -1,27 +1,21 @@
 import Link from 'next/link'
 import styles from '../../styles/components/ProductCard.module.scss'
 import productsDataTypes from './products'
-import { useDispatchCart, useCart } from '../helper/CartProvider'
-import contextProducts, { DispatchContext } from '../helper/CartProvider.d'
+import { useCart } from '../helper/CartProvider'
 
 export default function ProductCard({
   el,
 }: {
   el: productsDataTypes
 }): JSX.Element {
-  const dispatch: DispatchContext = useDispatchCart()
-  const addToCart = (item: contextProducts): void => {
-    dispatch({ type: 'ADD', item })
-  }
-
-  const { test } = useCart()
+  const { addItem } = useCart()
 
   return (
     <div className={styles.singleProduct}>
       <img loading='lazy' src={el.image} alt={el.title} />
       <p>{el.title}</p>
       <p>{el.price}</p>
-      <button onClick={() => test(el, 3)}>Add to cart</button>
+      <button onClick={() => addItem(el, 1)}>Add to cart</button>
       <br />
       <Link href='/product/[id]' as={`/product/${el.id}`}>
         <a>See product</a>
