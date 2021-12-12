b705039c9aa980af97a0b517bb1530ffb8c28797
Criiis <cristianocaires95@gmail.com>
2021-12-12 23:15:59 +0000

    update the cart single product to ouside of the return

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -6,24 +6,26 @@ import { contextProducts } from './helper/CartProvider.d'
 const Cart: NextPage = () => {
   const { state, removeItem, totalProductValue } = useCart()
 
+  const singleCartProducts = state.map((el: contextProducts, i: number) => (
+    <div key={i}>
+      <img loading='lazy' src={el.image} alt={el.title} width='150px' />
+      <p>{el.title}</p>
+      <p>
+        {el.price} £ | qty.{el?.quantity}
+      </p>
+      <button onClick={() => removeItem(i)}>remove from cart</button>
+      <br />
+      <Link href='/product/[id]' as={`/product/${el.id}`}>
+        <a>See product</a>
+      </Link>
+    </div>
+  ))
+
   return (
     <>
       <h1>cart page</h1>
       {state.length <= 0 ? <h2>cart empty</h2> : ``}
-      {state.map((el: contextProducts, i: number) => (
-        <div key={i}>
-          <img loading='lazy' src={el.image} alt={el.title} width='150px' />
-          <p>{el.title}</p>
-          <p>
-            {el.price} £ | qty.{el?.quantity}
-          </p>
-          <button onClick={() => removeItem(i)}>remove from cart</button>
-          <br />
-          <Link href='/product/[id]' as={`/product/${el.id}`}>
-            <a>See product</a>
-          </Link>
-        </div>
-      ))}
+      {singleCartProducts}
       <p>total: {totalProductValue()} £</p>
     </>
   )
