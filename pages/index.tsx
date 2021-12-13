eebdfe6087e60d8aea832ff827b031dbdec09e97
Criiis <cristianocaires95@gmail.com>
2021-12-13 22:16:25 +0000

    add a test of use state in the homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,13 @@
+import { useState } from 'react'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
+  const [test, setTest] = useState(false)
   return (
     <>
       <h1>HOME PAGE</h1>
+      <button onClick={() => setTest(!test)}>hello world!</button>
+      {test && <span>this is just a test to use useState</span>}
     </>
   )
 }
