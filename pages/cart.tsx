27ef0c53c67d9376bfe5173ef7b2e10d66d8fdc5
Criiis <cristianocaires95@gmail.com>
2021-12-13 00:16:52 +0000

    ideias

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -13,6 +13,25 @@ const Cart: NextPage = () => {
       <p>
         {el.price} £ | qty.{el?.quantity}
       </p>
+
+      {/* print values from 1 to 10 if number is miner them 10*/}
+      {/* otherwise print value until number of items +5 */}
+      {/* <select>
+        <option value='1'>1</option>
+        <option value='2'>2</option>
+        <option value='3'>3</option>
+        <option value='4'>4</option>
+        <option value='5'>5</option>
+        <option value='6'>6</option>
+        <option value='7'>7</option>
+        <option value='8'>8</option>
+        <option value='9'>9</option>
+        <option value='10'>10</option>
+      </select> */}
+
+      {/* could create a + and - button to update the quantity  <-------- should go for this*/}
+      {/* + 1 - */}
+
       <button onClick={() => removeItem(i)}>remove from cart</button>
       <br />
       <Link href='/product/[id]' as={`/product/${el.id}`}>
