3bd44c6e423b68f7730339528efe86b423298c6f
Criiis <cristianocaires95@gmail.com>
2021-12-13 22:18:19 +0000

    add the new funcionality

--- pages/cart.tsx
+++ pages/cart.tsx
@@ -4,35 +4,26 @@ import { useCart } from './helper/CartProvider'
 import { contextProducts } from './helper/CartProvider.d'
 
 const Cart: NextPage = () => {
-  const { state, removeItem, totalProductValue } = useCart()
+  const {
+    state,
+    removeItem,
+    totalProductValue,
+    addSingleQuantity,
+    removeSingleQuantity,
+  } = useCart()
 
   const singleCartProducts = state.map((el: contextProducts, i: number) => (
     <div key={i}>
       <img loading='lazy' src={el.image} alt={el.title} width='150px' />
       <p>{el.title}</p>
-      <p>
-        {el.price} £ | qty.{el?.quantity}
-      </p>
-
-      {/* print values from 1 to 10 if number is miner them 10*/}
-      {/* otherwise print value until number of items +5 */}
-      {/* <select>
-        <option value='1'>1</option>
-        <option value='2'>2</option>
-        <option value='3'>3</option>
-        <option value='4'>4</option>
-        <option value='5'>5</option>
-        <option value='6'>6</option>
-        <option value='7'>7</option>
-        <option value='8'>8</option>
-        <option value='9'>9</option>
-        <option value='10'>10</option>
-      </select> */}
-
-      {/* could create a + and - button to update the quantity  <-------- should go for this*/}
-      {/* + 1 - */}
-
-      <button onClick={() => removeItem(i)}>remove from cart</button>
+      <p>{el.price} £</p>
+      <button onClick={() => addSingleQuantity(el)}>+</button>
+      <span> Qty. {el?.quantity} </span>
+      {el?.quantity! > 1 && (
+        <button onClick={() => removeSingleQuantity(el)}>-</button>
+      )}{' '}
+      <br />
+      <button onClick={() => removeItem(el)}>remove from cart</button>
       <br />
       <Link href='/product/[id]' as={`/product/${el.id}`}>
         <a>See product</a>
@@ -43,7 +34,7 @@ const Cart: NextPage = () => {
   return (
     <>
       <h1>cart page</h1>
-      {state.length <= 0 ? <h2>cart empty</h2> : ``}
+      {state.length <= 0 && <h2>cart empty</h2>}
       {singleCartProducts}
       <p>total: {totalProductValue()} £</p>
     </>
