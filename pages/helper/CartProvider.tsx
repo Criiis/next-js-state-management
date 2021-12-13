5349216b15a3209dbd776e0189a3168693413e6c
Criiis <cristianocaires95@gmail.com>
2021-12-13 22:18:09 +0000

    add new funcionality to increase or decrease quantity of 1 item

--- pages/helper/CartProvider.tsx
+++ pages/helper/CartProvider.tsx
@@ -8,13 +8,13 @@ import {
   stateType,
   globalAction,
 } from './CartProvider.d'
-import productsDataTypes from '../components/products.d'
 import { useSetLocalStorage } from './localStorage'
 
 const ACTIONS: globalAction = {
-  //HAVE TO CREATE A TYPE FOR THIS
   ADD: 'ADD',
   REMOVE: 'REMOVE',
+  ADDSINGLE: 'ADDSINGLE',
+  REMOVESINGLE: 'REMOVESINGLE',
 }
 
 const CartDispatchContext = createContext({} as dispatchContext)
@@ -25,42 +25,55 @@ const cartStorage: string = 'cartStorage' // <- localStorage key
 const reducer = (state: stateType, action: action): stateType => {
   switch (action.type) {
     case ACTIONS.ADD:
-      //function to map and change the current product || and filter the false
-      const productAdded: (false | contextProducts)[] = state
-        .map((el) => {
-          if (el?.id === action.id) return false
-          return el
-        })
-        .filter((el: contextProducts | false) => el instanceof Object)
-      return [...productAdded, action.payload] as stateType //return this as object of all items, total price, total items in cart
+      const addIndex: number = state.findIndex((el) => el.id === action.id)
+      if (addIndex === -1) return [...state, action.payload]
+      state.splice(addIndex, 1)
+      return [...state, action.payload]
     case ACTIONS.REMOVE:
       const cartProducts: stateType = [...state]
-      cartProducts.splice(action.index, 1)
-      return cartProducts
+      const removeIndex: number = cartProducts.findIndex(
+        (el) => el.id === action.id
+      )
+      cartProducts.splice(removeIndex, 1)
+      return [...cartProducts]
+    case ACTIONS.ADDSINGLE:
+      return [...state]
+    case ACTIONS.REMOVESINGLE:
+      return [...state]
     default:
       throw new Error(`couldn't ${action}`)
   }
 }
 
-const CartProvider = ({ children }: { children?: React.ReactNode }) => {
+const CartProvider = ({ children }: { children: React.ReactNode }) => {
   const [localStorage, setLocalStorage] = useSetLocalStorage(cartStorage, [])
   const [state, dispatch] = useReducer(reducer, localStorage)
 
   //function to add items -> passing from CartContextState/useCart
   const addItem = (item: contextProducts, quantity = 1) => {
-    //item + qty
-    const payload: contextProducts = { ...item, quantity }
-    //check if the product exist on state
-    const stateItem = state.find((el: contextProducts) => el.id === payload.id)
-    //if product exist in the state get the quantity and add the new quantity to payload
-    if (stateItem) payload.quantity = quantity + stateItem?.quantity!
-    // return the action, id of the item and payload
-    dispatch({ type: ACTIONS.ADD, id: payload.id, payload })
+    const payload: contextProducts = { ...item, quantity } //item + qty
+    const stateItem = state.find((el: contextProducts) => el.id === payload.id) //check if the product exist on state
+    if (stateItem) payload.quantity = quantity + stateItem?.quantity! //if product exist in the state get the quantity and add the new quantity to payload
+    dispatch({ type: ACTIONS.ADD, id: payload.id, payload }) // return the action, id of the item and payload
   }
 
   //function to remove items -> passing from CartContextState/useCart
-  const removeItem = (index: number) => {
-    dispatch({ type: ACTIONS.REMOVE, index })
+  const removeItem = (payload: contextProducts) => {
+    dispatch({ type: ACTIONS.REMOVE, id: payload.id })
+  }
+
+  //increase the quantity of products
+  const addSingleQuantity = (payload: contextProducts) => {
+    const newQuantity: number = payload?.quantity! + 1
+    payload.quantity = newQuantity
+    dispatch({ type: ACTIONS.ADDSINGLE, payload })
+  }
+
+  //decrease the quantity of products
+  const removeSingleQuantity = (payload: contextProducts) => {
+    const newQuantity: number = payload?.quantity! - 1
+    payload.quantity = newQuantity
+    dispatch({ type: ACTIONS.ADDSINGLE, payload })
   }
 
   //total number of products
@@ -93,11 +106,12 @@ const CartProvider = ({ children }: { children?: React.ReactNode }) => {
           state,
           addItem,
           removeItem,
+          addSingleQuantity,
+          removeSingleQuantity,
           totalItemsCart,
           totalProductValue,
         }}
       >
-        {/*state will be the items and addItem will be the function | it will pass all to "useCart" as an object*/}
         {children}
       </CartContextState.Provider>
     </CartDispatchContext.Provider>
