4637a94222eb9ffbcf46de7014424f442f520c35
Criiis <cristianocaires95@gmail.com>
2021-12-14 23:22:54 +0000

    adding into the app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,17 @@ import type { AppProps } from 'next/app'
 import HeadPage from './components/Head'
 import Menu from './components/Menu'
 import CartProvider from './helper/CartProvider'
+import SavedItemsProvider from './helper/SavedItemsProvider'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <CartProvider>
-      <HeadPage />
-      <Menu />
-      <Component {...pageProps} />
-    </CartProvider>
+    <SavedItemsProvider>
+      <CartProvider>
+        <HeadPage />
+        <Menu />
+        <Component {...pageProps} />
+      </CartProvider>
+    </SavedItemsProvider>
   )
 }
 
