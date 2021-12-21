4d1280245762d1545e92310061d20d0566352a65
Criiis <cristianocaires95@gmail.com>
2021-12-21 21:25:20 +0000

    create types for saved items

--- pages/helper/SavedItemsProvider.d.ts
+++ pages/helper/SavedItemsProvider.d.ts
@@ -1,3 +1,30 @@
 import { contextProducts } from './CartProvider.d'
 
 export type stateType = contextProducts[] | []
+
+export type action =
+  | {
+      type: 'ADD'
+      payload: contextProducts
+    }
+  | {
+      type: 'REMOVE'
+      payload: contextProducts
+      id: contextProducts['id']
+    }
+
+export type dispatchContext = (_: action) => void
+
+export interface contextTypes {
+  state: stateType
+  addSavedItem: (payload: contextProducts) => void
+  removedSavedItem: (
+    payload: contextProducts,
+    id: contextProducts['id']
+  ) => void
+}
+
+export interface globalAction {
+  ADD: 'ADD'
+  REMOVE: 'REMOVE'
+}
