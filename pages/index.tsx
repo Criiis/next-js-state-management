1f1f8026d6ec268e66ded96d5fa3792ba76cdc7c
Criiis <cristianocaires95@gmail.com>
2021-12-22 23:04:11 +0000

    adding reducer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,66 @@
-import { useState } from 'react'
+import { useState, useReducer } from 'react'
 import type { NextPage } from 'next'
+// import { type } from 'os'
+
+//reducer actions keywords
+const actions: { increment: 'INCREMENT'; decrement: 'DECREMENT' } = {
+  increment: 'INCREMENT',
+  decrement: 'DECREMENT',
+}
+
+//reducer action type
+type reducerActions =
+  | { type: 'INCREMENT'; numb: number }
+  | { type: 'DECREMENT'; numb: number }
+
+//reducer
+function reducer(state: number, action: reducerActions): number {
+  switch (action.type) {
+    case actions.increment:
+      console.log(action)
+      return state + action.numb
+    case actions.decrement:
+      if (state === 0) return state
+      return state - action.numb
+    default:
+      throw new Error()
+  }
+}
 
 const Home: NextPage = () => {
   const [test, setTest] = useState<boolean>(false)
   const [counter, setCounter] = useState<number>(0)
+  const [state, dispatch] = useReducer(reducer, 0)
 
+  /**
+   * functionality for useState
+   * @param numb
+   * @returns number
+   */
+  //increase function for the counter -> useState
   const increase = (numb: number): void =>
     setCounter((prevNumber) => prevNumber + numb)
-
+  //decrease function for the counter -> useState
   const decrease = (numb: number): void => {
     if (!counter) return
     setCounter((prevNumber) => prevNumber - numb)
   }
+
+  /**
+   * functionality for useReducer
+   * @param numb
+   * @returns void
+   */
+  //increase function for the counter -> useReducer
+  const increaseUseReducer = (numb: number): void => {
+    dispatch({ type: actions.increment, numb })
+  }
+  //decrease function for the counter -> useReducer
+  const decreaseUseReducer = (numb: number): void => {
+    dispatch({ type: actions.decrement, numb })
+  }
+
+  console.log(state)
 
   return (
     <>
@@ -24,6 +73,11 @@ const Home: NextPage = () => {
       <button onClick={() => increase(1)}>+</button>
       {counter}
       <button onClick={() => decrease(1)}>-</button>
+      <br />
+      <br />
+      <button onClick={() => increaseUseReducer(1)}>+</button>
+      {state}
+      <button onClick={() => decreaseUseReducer(1)}>-</button>
     </>
   )
 }
