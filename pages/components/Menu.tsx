9fc7d5bbfd2a35a97ca054e9a4cc88c0206426a4
Criiis <cristianocaires95@gmail.com>
2021-12-22 22:08:16 +0000

    chaning few things in menu

--- pages/components/Menu.tsx
+++ pages/components/Menu.tsx
@@ -1,12 +1,15 @@
 import Link from 'next/link'
 import { useCart } from '../helper/CartProvider'
 import { useSavedItems } from '../helper/SavedItemsProvider'
+import { BsFillHeartFill, BsBasketFill } from 'react-icons/bs'
+import styles from '../../styles/components/Menu.module.scss'
+console.log(styles)
 
 export default function Menu(): JSX.Element {
   const { totalItemsCart } = useCart()
   const { state } = useSavedItems()
   return (
-    <nav className='header-nav'>
+    <nav className={styles.headerNav}>
       <li>
         <Link href='/'>
           <a>Home</a>
@@ -19,12 +22,18 @@ export default function Menu(): JSX.Element {
       </li>
       <li>
         <Link href='/cart'>
-          <a>cart {totalItemsCart()}</a>
+          <a className={styles.svgCartItems}>
+            <BsBasketFill />
+            <span className={styles.titleIcon}>{totalItemsCart()}</span>
+          </a>
         </Link>
       </li>
       <li>
         <Link href='/saved-items'>
-          <a>Saved Items {state.length}</a>
+          <a className={styles.svgSavedItems}>
+            <BsFillHeartFill />
+            <span className={styles.titleIcon}>{state.length}</span>
+          </a>
         </Link>
       </li>
     </nav>
