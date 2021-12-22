3c93a6936aea9485ac37663a7fd4872990a58fe2
Criiis <cristianocaires95@gmail.com>
2021-12-22 21:38:32 +0000

    playing with useEffect

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,28 @@ import { useState } from 'react'
 import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
-  const [test, setTest] = useState(false)
+  const [test, setTest] = useState<boolean>(false)
+  const [counter, setCounter] = useState<number>(0)
+
+  const increase = (numb: number): void =>
+    setCounter((prevNumber) => prevNumber + numb)
+
+  const decrease = (numb: number): void => {
+    if (!counter) return
+    setCounter((prevNumber) => prevNumber - numb)
+  }
 
   return (
     <>
       <h1>HOME PAGE</h1>
       <button onClick={() => setTest(!test)}>hello world!</button>
+      {/* !false is a condition and i'm setting the value of useState as the result of the condition */}
       {test && <span>this is just a test to use useState</span>}
+      <br />
+      <br />
+      <button onClick={() => increase(1)}>+</button>
+      {counter}
+      <button onClick={() => decrease(1)}>-</button>
     </>
   )
 }
