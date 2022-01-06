215362eab4308b07cdd69604bb2c793a0b3e62e1
Criiis <cristianocaires95@gmail.com>
2022-01-06 23:14:45 +0000

    start mocking the data

--- __tests__/products.test.ts
+++ __tests__/products.test.ts
@@ -1,21 +1,21 @@
 import { getStaticProps } from '../pages/products'
 
 //start doing mock of data
-;(global.fetch as any) = jest.fn(() =>
-  Promise.resolve({
-    json: () =>
-      Promise.resolve({
-        props: {
-          data: [
-            { title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops' },
-          ],
-        },
-      }),
-  })
-)
+// ;(global.fetch as any) = jest.fn(() =>
+//   Promise.resolve({
+//     json: () =>
+//       Promise.resolve({
+//         props: {
+//           data: [
+//             { title: 'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops' },
+//           ],
+//         },
+//       }),
+//   })
+// )
 
 test('testing api call for all products', async () => {
-  const data = await getStaticProps() 
+  const data = await getStaticProps()
   console.log(data)
   // expect(data.props.data[0].title).toBe(
   //   'Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops'
