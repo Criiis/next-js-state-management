48bad8915c90dcbc5a3e7cefec56628971aa5b32
Criiis <cristianocaires95@gmail.com>
2022-01-06 22:15:07 +0000

    delete initial testes

--- __tests__/formatNumerTwo.test.ts
+++
@@ -1,5 +0,0 @@
-export {}
-
-it('this should console log something in terminal2', () => {
-  console.log('test2 ')
-})
--- __tests__/formatSome.test.ts
+++
@@ -1,5 +0,0 @@
-export {}
-
-it('this should console log something in terminal', () => {
-  console.log('test')
-})
